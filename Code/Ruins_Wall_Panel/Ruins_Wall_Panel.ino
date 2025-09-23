5bea357f8638f09b66950d1ef48c2d30cd407353
FlorestalEnterprises <FlorestalEnterprises@gmail.com>
2025-09-23 16:02:08 -0400

    First commit; Setting up repo.

---
+++ Code/Ruins_Wall_Panel/Ruins_Wall_Panel.ino
@@ -0,0 +1,39 @@
+#define MATRIX_INPUT 2
+#define MOSFET_OUTPUT 3 
+#define PWM_PIN 11
+
+
+uint8_t brightness;
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("Program started.");
+
+  pinMode(MATRIX_INPUT, INPUT);
+  pinMode(MOSFET_OUTPUT, OUTPUT);
+  pinMode(PWM_PIN,OUTPUT);
+
+  brightness = 128;
+}
+
+
+void turnOnLED(){
+  digitalWrite(MOSFET_OUTPUT,HIGH);  
+}
+
+void turnOffLED(){
+  digitalWrite(MOSFET_OUTPUT,LOW);    
+}
+
+void drivePWM(){
+  analogWrite(PWM_PIN,brightness);  
+}
+void loop() {
+  if(!digitalRead(MATRIX_INPUT)){
+    Serial.println("Matrix conductor connected");
+    turnOnLED();
+    drivePWM();
+  } else {
+    turnOffLED();  
+  }
+}
