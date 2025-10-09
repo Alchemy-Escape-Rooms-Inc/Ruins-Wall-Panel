1a6367fb5ac01223d2904fd92c77458b9dc0383c
FlorestalEnterprises <FlorestalEnterprises@gmail.com>
2025-10-08 21:04:22 -0400

    Added a fourth test function.

--- Code/Test/Test.ino
+++ Code/Test/Test.ino
@@ -1,17 +1,18 @@
 
 #include <FastLED.h>
 
-#define NUM_LEDS 92
+#define LEDS_PER_ROW 35
+#define NUM_ROWS 3
+#define NUM_LEDS (LEDS_PER_ROW * NUM_ROWS)
 #define LEDS_PIN 2
 
 
-CRGB leds[NUM_LEDS];
+int brightness = 128;
 
-int count = 0;
+CRGB leds[NUM_LEDS];
 
 void test1() {
   //blink LEDs
-  FastLED.setBrightness(64);
   for (int i = 0; i < 5; i++) {
     fill_solid(leds, NUM_LEDS, CRGB::White);
     FastLED.show();
@@ -29,10 +30,10 @@ void test2() {
   for (int i = 0; i < NUM_LEDS; i++) {
     leds[i] = CRGB::White;
     FastLED.show();
-    delay(50);
+    delay(25);
     FastLED.clear();
     FastLED.show();
-    delay(50);
+    delay(25);
   }
 
   delay(500);
@@ -50,26 +51,40 @@ void test3() {
   fill_solid(leds, NUM_LEDS, CRGB::Blue);
   FastLED.show();
   delay(1000);
-  FastLED.clear();
-  FastLED.show();
-  delay(1000);
+}
+
+void test4() {
+    EVERY_N_MILLISECONDS(50) {
+      for (int i = 0; i < LEDS_PER_ROW/2; i++) {
+        leds[i].fadeToBlackBy(random(40));
+      }
+      FastLED.show();
+      Serial.println("50 milli");
+    }
+    Serial.println("Outside of 50 milli");
+  
 }
 
 void setup() {
   FastLED.addLeds<WS2812B, LEDS_PIN, GRB>(leds, NUM_LEDS);
   pinMode(LEDS_PIN, OUTPUT);
+
+  Serial.begin(9600);
+  FastLED.setBrightness(brightness);
+
+  randomSeed(analogRead(A0));
+  for (int i = 0; i < LEDS_PER_ROW/2; i++) {
+    leds[i].setRGB(random(256), random(256), random(256));
+  }
+  FastLED.show();
 }
 
 void loop() {
-  if (count < 3) {
-    FastLED.clear();
-    FastLED.show();
-    test1();
-    test2();
-    test3();
-    FastLED.clear();
-  } 
 
-  count++;
-  return;
+  //FastLED.clear();
+  //FastLED.show();
+  //test1();
+  //test2();
+  //test3();
+  test4();
 }
